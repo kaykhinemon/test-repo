751765744bf6e110b99af4989c6f2329c5dfa134
Kay Khine Mon Mon <Kay Khine Mon Mon@msi>
2025-03-03 12:00:07 +0630

    update exception

---
+++ exception-05/src/test/java/com/jdc/mkt/test/HandlerTest.java
@@ -0,0 +1,16 @@
+package com.jdc.mkt.test;
+
+import org.junit.jupiter.api.Test;
+
+import com.jdc.mkt.handler.Handler;
+
+public class HandlerTest {
+	
+	
+	@Test
+	void test() {
+		Handler handle = new Handler();
+		handle.useName();
+	}
+
+}
